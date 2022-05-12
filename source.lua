652056827b348880f9d7e085c07f3536c85e95af
Bingo <57237071+BingoIsaac@users.noreply.github.com>
2022-05-12 16:21:22 -0400

    Add functions
    SetLibrary
    SetWindow
    DoesExist

--- source.lua
+++ source.lua
@@ -1,9 +1,30 @@
 local Module = {}
 
-local OrionLib = nil
+local OrionLibInstance = nil
+local WindowInstance = nil
 
-function Module.SetLibrary(OrionLibInstance)
-	OrionLib = OrionLibInstance
+function Module.SetLibrary(OrionLib)
+	OrionLibInstance = OrionLib
+end
+
+function Module.SetWindow(Window)
+	WindowInstance = Window
+end
+
+function Module.DoesExist(Object)
+	if Object == "Library" then
+		if OrionLib ~= nil then
+			return true
+		else
+			return false
+		end
+	elseif Object == "Window" then
+		if Window ~= nil then
+			return true
+		else
+			return false
+		end
+	end
 end
 
 return Module
