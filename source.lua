0aa17c1518c0b6a15a298e8e05592ddf83740618
Bingo <57237071+BingoIsaac@users.noreply.github.com>
2022-05-12 18:22:45 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,14 +1,50 @@
+-- // Preload Services
+local CoreGui = game:GetService("CoreGui")
+
+-- // Module Table
 local Module = {}
 
+-- // Core Orion Elements
 local OrionLibInstance = nil
-local WindowInstance = nil
+local OrionInstance = nil
+
+-- // Orion UI Elements
+
+local TitleInstance = nil
+
+-- // Core Functions
+
+function Module.SetOrion()
+	OrionInstance = CoreGui:FindFirstChild("Orion")
+end
 
 function Module.SetLibrary(OrionLib)
 	OrionLibInstance = OrionLib
 end
 
-function Module.SetWindow(Window)
-	WindowInstance = Window
+function Module:MakeWindow(Table)
+	local Name = Table.Name or "Orogo"
+	local HidePremium = Table.HidePremium or false
+	local SaveConfig = Table.SaveConfig or nil
+	local ConfigFolder = Table.ConfigFolder or nil
+	local Window = OrionLibInstance:MakeWindow({
+		Name = Name
+		HidePremium = HidePremium,
+		SaveConfig = SaveConfig,
+		ConfigFolder = ConfigFolder
+	})
+	
+	for Index, Label in pairs(OrionInstance:GetDescendants()) do
+		if Label:IsA("TextLabel") and Label.Text == Name then
+			TitleInstance = Label
+		end
+	end
+	
+	return Window
+end
+
+function Module:ChangeWindowTitle(Title)
+	TitleInstance.Text = Title
 end
 
 function Module.DoesExist(Object)
@@ -18,8 +54,8 @@ function Module.DoesExist(Object)
 		else
 			return false
 		end
-	elseif Object == "Window" then
-		if WindowInstance ~= nil then
+	elseif Object == "Orion" then
+		if OrionInstance ~= nil then
 			return true
 		else
 			return false
