ed8cc2f65261a8c36004e77bf4fed8bc90729f30
Bingo <57237071+BingoIsaac@users.noreply.github.com>
2022-05-29 20:52:32 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,5 +1,10 @@
 -- // Preload Services
 local CoreGui = game:GetService("CoreGui")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+--// Debug
+-- no debug yet
 
 -- // Module Table
 local Module = {}
@@ -10,10 +15,16 @@ local OrionInstance = nil
 
 -- // Orion UI Elements
 local TitleInstance = nil
+local RankInstance = nil
+
+--// Realization
+local IsPremiumHidden = false
 
 -- // Core Functions
-function Module.SetOrion()
-	OrionInstance = CoreGui:FindFirstChild("Orion")
+function Module.SyncOrion()
+    if CoreGui:FindFirstChild("Orion") then
+    	OrionInstance = CoreGui.Orion
+    end
 end
 
 function Module.SetLibrary(OrionLib)
@@ -21,22 +32,28 @@ function Module.SetLibrary(OrionLib)
 end
 
 function Module:MakeWindow(Table)
-	local Name = Table.Name or "Orogo"
-	local HidePremium = Table.HidePremium or false
-	local SaveConfig = Table.SaveConfig or nil
-	local ConfigFolder = Table.ConfigFolder or nil
+    local TitleOfWindow = Table["Name"] or "Orogo"
+    IsPremiumHidden = Table["HidePremium"] or false
 	local Window = OrionLibInstance:MakeWindow({
-		Name = Name
-		HidePremium = HidePremium,
-		SaveConfig = SaveConfig,
-		ConfigFolder = ConfigFolder
+		Name = TitleOfWindow,
+		HidePremium = IsPremiumHidden,
+		SaveConfig = Table["SaveConfig"] or nil,
+		ConfigFolder = Table["ConfigFolder"] or nil
 	})
 	
 	for Index, Label in pairs(OrionInstance:GetDescendants()) do
-		if Label:IsA("TextLabel") and Label.Text == Name then
+		if Label:IsA("TextLabel") and Label.Text == TitleOfWindow then
 			TitleInstance = Label
 		end
 	end
+	
+	if not IsPremiumHidden then
+    	for Index, Label in pairs(OrionInstance:GetDescendants()) do
+    		if Label:IsA("TextLabel") and (Label.Text == "Standard" or Label.Text == "Premium") then
+    			RankInstance = Label
+    		end
+    	end
+    end
 	
 	return Window
 end
@@ -45,20 +62,24 @@ function Module:ChangeWindowTitle(Title)
 	TitleInstance.Text = Title
 end
 
-function Module.DoesExist(Object)
-	if Object == "Library" then
-		if OrionLibInstance ~= nil then
-			return true
-		else
-			return false
-		end
-	elseif Object == "Orion" then
-		if OrionInstance ~= nil then
-			return true
-		else
-			return false
-		end
-	end
+function Module.SetUserRanks(Table)
+    if not IsPremiumHidden then
+        for Id, Rank in pairs(Table) do
+            if LocalPlayer.UserId == Id then
+                RankInstance.Text = Rank
+            end
+        end
+    else
+        warn("Premium is set to hidden and cannot be set. (Orogo)")
+    end
+end
+
+function Module:ChangeLiveRank(Rank)
+    if not IsPremiumHidden then
+        RankInstance.Text = Rank
+    else
+        warn("Premium is set to hidden and cannot be set. (Orogo)")
+    end
 end
 
 return Module
